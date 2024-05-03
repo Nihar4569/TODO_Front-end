968414e66e0d0825b96fccecd3845ea272f32263
Nihar4569 <nihar4569@gmail.com>
2024-05-04 03:52:09 +0530

    backend API added

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import "./styles/app.scss";
 import { createContext } from 'react';
 
-export const server = " http://localhost:4000/api/v1";
+export const server = "https://nodejs-todo-backend-rpw6.onrender.com/api/v1";
 
 export const Context = createContext({isAuthenticated:false});
 
